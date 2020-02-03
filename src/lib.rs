19b90e858495e0ad81fcfd66e1f2cdf584dc7ad9
Otávio Pace <otaviopp8@gmail.com>
2020-02-02 22:29:40 -0300

    mean: add function for positive integers

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,14 @@
+pub fn mean(data: Vec<usize>) -> f32 {
+    data.iter().fold(0, |acc, curr| acc + curr) as f32 / data.len() as f32
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::mean;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_mean_positive_integers() {
+        let result = mean(vec![0, 1, 2, 3, 4, 5]);
+        assert_eq!(result, 2.5);
     }
 }
