d2bfdf99280112a534c4ef38e93f49e47293a9bf
Otávio Pace <otaviopp8@gmail.com>
2020-02-06 00:42:40 -0300

    lib: improve efficiency by removing clones and using references

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,7 @@ macro_rules! number_trait_impl {
 }
 number_trait_impl!(Number for u8 u16 u32 i8 i16 i32 f32 f64);
 
-pub fn mean<T: Number>(data: Vec<T>) -> f64 {
+pub fn mean<T: Number>(data: &Vec<T>) -> f64 {
     (data.iter().fold(T::default(), |acc, curr| acc + *curr)).into() / data.len() as f64
 }
 
@@ -26,7 +26,7 @@ pub enum Mode<T> {
 use std::str::FromStr;
 use std::string::ToString;
 
-pub fn mode<T: Number + ToString + FromStr>(data: Vec<T>) -> Mode<T> {
+pub fn mode<T: Number + ToString + FromStr>(data: &Vec<T>) -> Mode<T> {
     let mut values_to_frequency: HashMap<String, usize> = HashMap::new();
 
     for value in data {
@@ -130,7 +130,7 @@ pub fn mode<T: Number + ToString + FromStr>(data: Vec<T>) -> Mode<T> {
     }
 }
 
-pub fn median<T: Number>(data: Vec<T>) -> Option<f64> {
+pub fn median<T: Number>(data: &Vec<T>) -> Option<f64> {
     let is_odd = data.len() % 2 != 0;
     if data.len() == 0 {
         None
@@ -139,22 +139,22 @@ pub fn median<T: Number>(data: Vec<T>) -> Option<f64> {
     } else {
         let middle1 = data[data.len() / 2 - 1];
         let middle2 = data[data.len() / 2];
-        let middle_mean = mean(vec![middle1.into(), middle2.into()]);
+        let middle_mean = mean(&vec![middle1.into(), middle2.into()]);
         Some(middle_mean)
     }
 }
 
-pub fn variance<T: Number>(data: Vec<T>) -> f64 {
-    let list_mean = mean(data.clone());
+pub fn variance<T: Number>(data: &Vec<T>) -> f64 {
+    let list_mean = mean(&data);
     let it = data.iter();
     let differences = it.map(|v| ((*v).into() - list_mean).abs());
     let powered_differences = differences.map(|d| d * d);
     let powered_differences: Vec<f64> = powered_differences.collect();
-    mean(powered_differences)
+    mean(&powered_differences)
 }
 
-pub fn standard_deviation<T: Number>(data: Vec<T>) -> f64 {
-    variance(data.iter().map(|a| (*a).into()).collect()).sqrt()
+pub fn standard_deviation<T: Number>(data: &Vec<T>) -> f64 {
+    variance(&data.iter().map(|a| (*a).into()).collect()).sqrt()
 }
 
 #[cfg(test)]
@@ -163,219 +163,219 @@ mod tests {
 
     #[test]
     fn test_mean_positive_integers() {
-        let result = mean(vec![0, 1, 2, 3, 4, 5]);
+        let result = mean(&vec![0, 1, 2, 3, 4, 5]);
         assert_eq!(result, 2.5);
     }
 
     #[test]
     fn test_mean_negative_integers() {
-        let result = mean(vec![0, -1, -2, -3, -4, -5]);
+        let result = mean(&vec![0, -1, -2, -3, -4, -5]);
         assert_eq!(result, -2.5);
     }
 
     #[test]
     fn test_mean_mixed_integers() {
-        let result = mean(vec![0, -1, 2, -3, 4, -5]);
+        let result = mean(&vec![0, -1, 2, -3, 4, -5]);
         assert_eq!(result, -0.5);
     }
 
     #[test]
     fn test_mean_positive_floats() {
-        let result = mean(vec![0.4, 1.2, 2.8, 3.3, 4.9, 5.2]);
+        let result = mean(&vec![0.4, 1.2, 2.8, 3.3, 4.9, 5.2]);
         assert_eq!(result, 2.966666666666667);
     }
 
     #[test]
     fn test_mode_positive_integers_none() {
-        let result = mode(vec![0, 1, 2, 3, 4, 5]);
+        let result = mode(&vec![0, 1, 2, 3, 4, 5]);
         assert_eq!(result, Mode::None);
     }
 
     #[test]
     fn test_mode_none_with_empty_list() {
         let empty_list: Vec<i32> = vec![];
-        let result = mode(empty_list);
+        let result = mode(&empty_list);
         assert_eq!(result, Mode::None);
     }
 
     #[test]
     fn test_mode_positive_integers_unimodal() {
-        let result = mode(vec![0, 1, 2, 3, 1]);
+        let result = mode(&vec![0, 1, 2, 3, 1]);
         assert_eq!(result, Mode::Unimodal(1));
     }
 
     #[test]
     fn test_mode_positive_integers_bimodal() {
-        let result = mode(vec![0, 1, 3, 3, 1]);
+        let result = mode(&vec![0, 1, 3, 3, 1]);
         assert_eq!(result, Mode::Bimodal(1, 3));
     }
 
     #[test]
     fn test_mode_positive_integers_trimodal() {
-        let result = mode(vec![0, 1, 3, 3, 1, 0]);
+        let result = mode(&vec![0, 1, 3, 3, 1, 0]);
         assert_eq!(result, Mode::Trimodal(0, 1, 3));
     }
 
     #[test]
     fn test_mode_positive_integers_multimodal() {
-        let result = mode(vec![0, 1, 3, 3, 1, 0, 2, 2]);
+        let result = mode(&vec![0, 1, 3, 3, 1, 0, 2, 2]);
         assert_eq!(result, Mode::Multimodal(vec![0, 1, 2, 3]));
     }
 
     #[test]
     fn test_mode_negative_integers_none() {
-        let result = mode(vec![0, -1, -2, -3, -4, -5]);
+        let result = mode(&vec![0, -1, -2, -3, -4, -5]);
         assert_eq!(result, Mode::None);
     }
 
     #[test]
     fn test_mode_negative_integers_unimodal() {
-        let result = mode(vec![0, -1, -2, -3, -1]);
+        let result = mode(&vec![0, -1, -2, -3, -1]);
         assert_eq!(result, Mode::Unimodal(-1));
     }
 
     #[test]
     fn test_mode_negative_integers_bimodal() {
-        let result = mode(vec![0, -1, -3, -3, -1]);
+        let result = mode(&vec![0, -1, -3, -3, -1]);
         assert_eq!(result, Mode::Bimodal(-1, -3));
     }
 
     #[test]
     fn test_mode_negative_integers_trimodal() {
-        let result = mode(vec![0, -1, -3, -3, -1, 0]);
+        let result = mode(&vec![0, -1, -3, -3, -1, 0]);
         assert_eq!(result, Mode::Trimodal(-1, -3, 0));
     }
 
     #[test]
     fn test_mode_negative_integers_multimodal() {
-        let result = mode(vec![-0, -1, -3, -3, -1, 0, -2, -2]);
+        let result = mode(&vec![-0, -1, -3, -3, -1, 0, -2, -2]);
         assert_eq!(result, Mode::Multimodal(vec![-1, -2, -3, 0]));
     }
 
     #[test]
     fn test_mode_mixed_integers_unimodal() {
-        let result = mode(vec![0, -1, 2, 3, -1]);
+        let result = mode(&vec![0, -1, 2, 3, -1]);
         assert_eq!(result, Mode::Unimodal(-1));
     }
 
     #[test]
     fn test_mode_floats_unimodal() {
-        let result = mode(vec![0.0, 1.0, 2.0, 3.0, 1.0]);
+        let result = mode(&vec![0.0, 1.0, 2.0, 3.0, 1.0]);
         assert_eq!(result, Mode::Unimodal(1.0));
     }
 
     #[test]
     fn test_mode_floats_bimodal() {
-        let result = mode(vec![0.0, 1.5, 3.2, 3.2, 1.5]);
+        let result = mode(&vec![0.0, 1.5, 3.2, 3.2, 1.5]);
         assert_eq!(result, Mode::Bimodal(1.5, 3.2));
     }
 
     #[test]
     fn test_mode_floats_trimodal() {
-        let result = mode(vec![0.4, 1.1, 3.9, 3.9, 1.1, 0.4]);
+        let result = mode(&vec![0.4, 1.1, 3.9, 3.9, 1.1, 0.4]);
         assert_eq!(result, Mode::Trimodal(0.4, 1.1, 3.9));
     }
 
     #[test]
     fn test_mode_floats_multimodal() {
-        let result = mode(vec![0.3, 1.7, 3.5, 3.5, 1.7, 0.3, 2.6, 2.6]);
+        let result = mode(&vec![0.3, 1.7, 3.5, 3.5, 1.7, 0.3, 2.6, 2.6]);
         assert_eq!(result, Mode::Multimodal(vec![0.3, 1.7, 2.6, 3.5]));
     }
 
     #[test]
     fn test_median_empty_list() {
         let empty_list: Vec<i32> = vec![];
-        let result = median(empty_list);
+        let result = median(&empty_list);
         assert_eq!(result, None);
     }
 
     #[test]
     fn test_median_odd_length_positive_integers() {
-        let result = median(vec![7, 8, 3, 9, 22]);
+        let result = median(&vec![7, 8, 3, 9, 22]);
         assert_eq!(result, Some(3.0));
     }
 
     #[test]
     fn test_median_even_length_positive_integers() {
-        let result = median(vec![7, 8, 3, 6, 22, 42]);
+        let result = median(&vec![7, 8, 3, 6, 22, 42]);
         assert_eq!(result, Some(4.5));
     }
 
     #[test]
     fn test_median_odd_length_negative_integers() {
-        let result = median(vec![-7, -8, -3, -9, -22]);
+        let result = median(&vec![-7, -8, -3, -9, -22]);
         assert_eq!(result, Some(-3.0));
     }
 
     #[test]
     fn test_median_even_length_negative_integers() {
-        let result = median(vec![-7, -8, -3, -6, -22, -42]);
+        let result = median(&vec![-7, -8, -3, -6, -22, -42]);
         assert_eq!(result, Some(-4.5));
     }
 
     #[test]
     fn test_median_even_length_mixed_integers() {
-        let result = median(vec![7, -8, -3, 6, -22, -42]);
+        let result = median(&vec![7, -8, -3, 6, -22, -42]);
         assert_eq!(result, Some(1.5));
     }
 
     #[test]
     fn test_variance_positive_integers() {
-        let result = variance(vec![14, 22, 19, 21, 21]);
+        let result = variance(&vec![14, 22, 19, 21, 21]);
         assert_eq!(result, 8.239999999999998);
     }
 
     #[test]
     fn test_variance_negative_integers() {
-        let result = variance(vec![-22, -21, -20, -18, -15]);
+        let result = variance(&vec![-22, -21, -20, -18, -15]);
         assert_eq!(result, 6.159999999999999);
     }
 
     #[test]
     fn test_variance_positive_floats() {
-        let result = variance(vec![7.5, 6.0, 7.0, 6.5, 8.0]);
+        let result = variance(&vec![7.5, 6.0, 7.0, 6.5, 8.0]);
         assert_eq!(result, 0.5);
     }
 
     #[test]
     fn test_variance_negative_floats() {
-        let result = variance(vec![-7.5, -5.0, -8.0, -8.3, -6.0]);
+        let result = variance(&vec![-7.5, -5.0, -8.0, -8.3, -6.0]);
         assert_eq!(result, 1.5864000000000005);
     }
 
     #[test]
     fn test_variance_mixed_floats() {
-        let result = variance(vec![-7.5, 5.0, -8.0, 8.3, -6.0]);
+        let result = variance(&vec![-7.5, 5.0, -8.0, 8.3, -6.0]);
         assert_eq!(result, 47.33840000000001);
     }
 
     #[test]
     fn test_standard_deviation_positive_integers() {
-        let result = standard_deviation(vec![14, 22, 19, 21, 21]);
+        let result = standard_deviation(&vec![14, 22, 19, 21, 21]);
         assert_eq!(result, 2.8705400188814645);
     }
 
     #[test]
     fn test_standard_deviation_negative_integers() {
-        let result = standard_deviation(vec![-22, -21, -20, -18, -15]);
+        let result = standard_deviation(&vec![-22, -21, -20, -18, -15]);
         assert_eq!(result, 2.481934729198171);
     }
 
     #[test]
     fn test_standard_deviation_positive_floats() {
-        let result = standard_deviation(vec![7.5, 6.0, 7.0, 6.5, 8.0]);
+        let result = standard_deviation(&vec![7.5, 6.0, 7.0, 6.5, 8.0]);
         assert_eq!(result, 0.7071067811865476);
     }
 
     #[test]
     fn test_standard_deviation_negative_floats() {
-        let result = standard_deviation(vec![-7.5, -5.0, -8.0, -8.3, -6.0]);
+        let result = standard_deviation(&vec![-7.5, -5.0, -8.0, -8.3, -6.0]);
         assert_eq!(result, 1.2595237195067033);
     }
 
     #[test]
     fn test_standard_deviation_mixed_floats() {
-        let result = standard_deviation(vec![-7.5, 5.0, -8.0, 8.3, -6.0]);
+        let result = standard_deviation(&vec![-7.5, 5.0, -8.0, 8.3, -6.0]);
         assert_eq!(result, 6.880290691533316);
     }
 }
